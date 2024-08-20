c99dc256c0d443efaf3d9f6770cce1eec2084e98
mahmoudelshirbeny1 <elshirbenymahmoud@gmail.com>
2024-08-20 17:14:36 +0300

    Add files via upload

---
+++ First-session.js
@@ -0,0 +1,21 @@
+var x = 30;
+function hello() {
+  console.log(x);
+  x = 10;
+  console.log(x);
+}
+hello();
+console.log(x);
+
+let y = 3;
+let z = "mahmoud";
+let user = { name: "khaled", age: 26 };
+let students = [10, 20, 30, 50];
+typeof x;
+typeof y;
+typeof z;
+typeof user;
+typeof students;
+
+y = y + students;
+console.log(y);
